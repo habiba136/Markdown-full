d73f0e440d372ddd3be75dfa9f0b95fadb12f77e
Habiba Panwar <habibapanwar786@gmail.com>
2023-10-23 07:37:41 -0700

    Code

---
+++ Markdown/Code.py
@@ -0,0 +1,165 @@
+echo echo # 1. Headings
+How give headings in markdown files?
+# Heading 1
+>This is a first heading.
+## Heading 2
+>This is a second heading.
+### Heading 3
+* > 3rd heading.
+* >With three hashese.
+* >hashess
+#### Heading 4
+Total six headings
+# 2.Block of words
+> Blocks of words.\
+> Blocks
+> is used for linebreak or double enter.
+This is a normal text in markdown.
+> This is bloack of text.
+> my cat i love her
+# 3.Line breaks
+> For breaking line.\
+**This is a 40 days course of python with habiba ramzan. Im  learning python from codanics.**
+# 4.Combine two things.
+Bloack of words and headings.
+>## Heading 2
+# 5.Face of text
+**Bold**
+*Italic* ****Bold and Italic***
+or you can use
+__bold__
+_Italic_
+# 6.Bullets points
+- day 1
+- day 2
+- day 3
+- day 4
+- day 5
+  - day-5 A
+       - day-5-1
+  - day-5 B
+  > numbering of lists
+  1. day 1
+  2. day 2
+  3. day 3
+  4. day 4
+  5. day 5
+
+## using * or + ##
+> Both can be used for bold text **\
+> or * for lists e.g bellow.
+* A
++ b
++ +c
+# 7 Linr breaks or page breaks
+> This is one page.
+# 8. Links and hyperlinks
+<https://www.coursera.org/learn/python?action=enroll&authMode=signup>
+
+**hyperlinks**:
+[The playlist of of is here:](https://www.coursera.org/learn/python?action=enroll&authMode=signup)
+
+<!-- [The whole Play list is here:](https://www.coursera.org/learn/python?action=enroll&authMode=signup) -->
+<!-- [app]:https://www.coursera.org/learn/python?action=enroll&authMode=signup
+The full app code is here.[here][app] -->
+
+[codanics]:https://www.coursera.org/learn/python?action=enroll&authMode=signup
+
+This is a whole cours of [here][codanics] 
+# 9.Images and figures
+To join this playliat scanqw code:
+![QR](qr.png)
+> how to comment out in makdown in shortcut
+**online picture**
+![codanic](https://www.coursera.org/learn/python?action=enroll&authMode=signup)
+
+>we can also add pdf
+# 10. Adding code blocks(In different colors code)
+```c#
+To print a string use `print("hey!")`
+To print names in pythin `print("your name!)`
+
+```
+```html
+<html>
+  <body>
+    <p>
+      Catiee in my nick name.
+      </p>
+  </body>
+  </html>
+```
+> code according to your language.
+
+
+```r
+
+x = 5+6
+
+```
+```c
+
+2+3
+
+```
+# 11. Adding Tables
+|sepcies | petal_length | sepal_length|
+| :------ | :------------: | :-----------: |
+| virginica | 18.2 | 19.2 |
+| setoca | 15.1 | 17.2 |
+| versicolor | 12.2 | 12.2 |
+
+# 12. Content
+[1-Headings](#1-headings)\
+[2-block](#2block-of-words)\
+[3-line](#3line-breaks)\
+[4-two](#4combine-two-things)\
+[5-face](#5face-of-text)\
+[6-bullets](#6bullets-points)\
+[7-line](#7-linr-breaks-or-page-breaks)\
+[8-links](#8-links-and-hyperlinks)\
+[9-images](#9images-and-figures)\
+[10-code](#10-adding-code-bloks)\
+[11-table](#11-adding-tables)\
+[12-content](#12-content)\
+[13-ext](#13-install-extensions)
+
+
+# 13. install extensions
++ **markdown all in one**
++ **markdownpdf**
++ **markdown short cuts**
++ **markdownlint**
++ *Markdown*
+
+```c#
+a = b+c
+```
+```c#
+print("habiba")
+// pyhton code
+```
+**Links again for batter understanding.**
++ Method 1
+<https://www.electronicsforu.com/electronics-projects/emg-based-wrong-posture-muscle-strain-detector>
+
++ Method 2
+[codanics:](https://www.electronicsforu.com/electronics-projects/emg-based-wrong-posture-muscle-strain-detector)
++ Method 3
+[codanicsplaylists]:https://www.electronicsforu.com/electronics-projects/emg-based-wrong-posture-muscle-strain-detector
+click [here][codanics]\
+*More*.
+[link](https://www.electronicsforu.com/electronics-projects/emg-based-wrong-posture-muscle-strain-detector)
+
+![QR](qr.png)
+[]**Scan me**
+# 14.Tables
+| saad | Adeela | Hibu |
+| ---- | ------ | ---- |
+| :Catiee: | :Mano:| **:Hibu:**|
+# 15.Images
+![image](qr.png)
+> ~~hibu\
+> catiee~~\
+> ~~hiba\
+> **Thank you so much..**
